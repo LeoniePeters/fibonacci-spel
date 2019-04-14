cfe4e617fbde4d51b2c7b68513b0814c62c05209
LeoniePeters <leoniepeters@gmail.com>
2019-04-14 15:29:03 +0200

    Add files for FibonacciCheck and write test

---
+++ src/FibonacciCheck/FibonacciCheck.spec.js
@@ -0,0 +1,13 @@
+import FibonacciCheck from './FibonacciCheck'
+
+test('Returns false when not Fibonacci sequence', () => {
+  expect(FibonacciCheck([0, 1, 1, 2, 5, 7, 3, 5, 8, 13, 25])).toBe(false)
+})
+
+test('Returns true when contains Fibonacci sequence', () => {
+  expect(FibonacciCheck([0, 1, 1, 2, 3, 5, 3, 5, 8, 13, 25])).toBe(true)
+})
+
+test('Returns true when contains Fibonacci sequence', () => {
+  expect(FibonacciCheck([5, 3, 5, 8, 13, 25, 144, 233, 377, 610, 987])).toBe(true)
+})
