a0b7276640d4f07b4b1cf017f7191ff6cb8a2b76
LeoniePeters <leoniepeters@gmail.com>
2019-04-14 10:08:17 +0200

    Write loop that creates grid of arrays and objects

--- src/components/FibonacciContainer.js
+++ src/components/FibonacciContainer.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import Fibonacci from './Fibonacci'
 
 class FibonacciContainer extends Component {
+  state = {}
+
+  componentWillMount() {
+    const grid = [];
+    for (let i = 0; i < 50; i++) {
+      grid.push([]);
+      for (let j = 0; j < 50; j++) {
+        grid[i].push({ row: i, column: j, number: 0 })
+      }
+    }
+    this.setState({ grid })
+
+  }
+
   render() {
-    <Fibonacci />
+    console.log(this.state)
+    return (
+      <Fibonacci />
+    )
   }
 }
 
