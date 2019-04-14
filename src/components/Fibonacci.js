e76f1d2c86e60010740df9c55e0007d2cef2cb84
LeoniePeters <leoniepeters@gmail.com>
2019-04-14 09:17:22 +0200

    Create container and presentational components for game

---
+++ src/components/Fibonacci.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Fibonacci(props) {
+
+  return (
+    <div></div>
+  )
+}
---
+++ src/components/FibonacciContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Fibonacci from './Fibonacci'
+
+class FibonacciContainer extends Component {
+  render() {
+    <Fibonacci />
+  }
+}
+
+export default FibonacciContainer;
