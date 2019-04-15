ba504fd49e752bf3fc896a1004582c29f40c597f
LeoniePeters <leoniepeters@gmail.com>
2019-04-15 13:01:25 +0200

    change tests to test new FibonacciCheck function

--- src/FibonacciCheck/FibonacciCheck.spec.js
+++ src/FibonacciCheck/FibonacciCheck.spec.js
@@ -3,17 +3,33 @@ import FibonacciCheck from './FibonacciCheck'
 test('Returns false when not Fibonacci sequence', () => {
   const input = [{ number: 0 }, { number: 1 }, { number: 1 }, { number: 2 },
   { number: 5 }, { number: 7 }, { number: 3 }, { number: 5 }]
-  expect(FibonacciCheck(input)).toBe(false)
+  const func = jest.fn(arg => FibonacciCheck(arg))
+  func(input);
+  expect(func).toHaveReturnedWith([])
 })
 
-test('Returns true when contains Fibonacci sequence', () => {
+test('Returns false Fibonacci sequence of 5, but starting with zero', () => {
   const input = [{ number: 0 }, { number: 1 }, { number: 1 }, { number: 2 },
-  { number: 3 }, { number: 5 }, { number: 3 }, { number: 5 }]
-  expect(FibonacciCheck(input)).toBe(1)
+  { number: 3 }, { number: 3 }, { number: 5 }, { number: 5 }]
+  const func = jest.fn(arg => FibonacciCheck(arg))
+  func(input);
+  expect(func).toHaveReturnedWith([])
 })
 
-test('Returns true when contains Fibonacci sequence', () => {
+test('Returns array of string when contains 1 Fibonacci sequence', () => {
   const input = [{ number: 8 }, { number: 13 }, { number: 25 }, { number: 144 },
   { number: 233 }, { number: 377 }, { number: 610 }, { number: 987 }]
-  expect(FibonacciCheck(input)).toBe(3)
+  const func = jest.fn(arg => FibonacciCheck(arg))
+  func(input);
+  expect(func).toHaveReturnedWith(['3-5'])
+})
+
+test('Returns array of strings when contains more Fibonacci sequences', () => {
+  const input = [{ number: 8 }, { number: 13 }, { number: 25 }, { number: 144 },
+  { number: 233 }, { number: 377 }, { number: 610 }, { number: 987 },
+  { number: 0 }, { number: 1 }, { number: 1 }, { number: 2 }, { number: 3 },
+  { number: 5 }, { number: 8 }, { number: 13 }, { number: 21 }]
+  const func = jest.fn(arg => FibonacciCheck(arg))
+  func(input);
+  expect(func).toHaveReturnedWith(['3-5', '9-8'])
 })
