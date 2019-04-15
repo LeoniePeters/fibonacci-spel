e50ecd2b92cc72bb59f7dbc0e6135db0ab94c43c
LeoniePeters <leoniepeters@gmail.com>
2019-04-15 21:57:00 +0200

    cleanup Fibonacci.js

--- src/components/Fibonacci.js
+++ src/components/Fibonacci.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function renderCell(cellObject, handleOnClick) {
   return (
-    <td className='cellData' id={`${cellObject.row}-${cellObject.column}`}
+    <td className='cellData'
+      id={`${cellObject.row}-${cellObject.column}`}
       key={`${cellObject.row}-${cellObject.column}`}
-      onClick={handleOnClick} style={{ animation: cellObject.color }}>
+      onClick={handleOnClick}
+      style={{ animation: cellObject.color }}>
       {cellObject.number === 0 ? '' : cellObject.number}
     </td>
   )
@@ -16,9 +18,11 @@ export default function Fibonacci({ grid, handleOnClick }) {
       <h1 id='fibonacciTitle'>Fibonacci spel</h1>
       <table className='grid-container'>
         <tbody>
-          {grid.map((gridRow, gridRowIndex) => <tr className='tableRow'
-            key={gridRowIndex}>
-            {gridRow.map(gridCell => renderCell(gridCell, handleOnClick))}</tr>)}
+          {grid.map((gridRow, gridRowIndex) =>
+            <tr className='tableRow'
+              key={gridRowIndex}>
+              {gridRow.map(gridCell =>
+                renderCell(gridCell, handleOnClick))}</tr>)}
         </tbody>
       </table>
     </div>
